77a5dc1ee6e62afc402201412dd749f1f07300a3
Dmitri Iurceac <dmitrilurceac93@gmail.com>
2019-10-27 09:19:35 +0000

    schema defined

--- django_graphql_movies/schema.py
+++ django_graphql_movies/schema.py
@@ -10,3 +10,5 @@ class Mutation(django_graphql_movies.movies.schema.Mutation, graphene.ObjectType
     # This class will inherit from multiple Queries
     # as we begin to add more apps to our project
     pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
