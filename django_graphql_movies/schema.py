df9226fd6887648b5d5c2c71a015201a9f63b9f5
Dmitri Iurceac <dmitrilurceac93@gmail.com>
2019-10-27 09:18:36 +0000

    Query type for entire projectd

---
+++ django_graphql_movies/schema.py
@@ -0,0 +1,7 @@
+import graphene
+import django_graphql_movies.movies.schema
+
+class Query(django_graphql_movies.movies.schema.Query, graphene.ObjectType):
+    # This class will inherit from multiple Queries
+    # as we begin to add more apps to our project
+    pass
