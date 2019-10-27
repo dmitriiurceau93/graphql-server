2a377eaf7fdf5787f63bec025a2f6bc8db1cdb16
Dmitri Iurceac <dmitrilurceac93@gmail.com>
2019-10-27 08:51:34 +0000

    Initial Query Type for movies app

--- django_graphql_movies/movies/schema.py
+++ django_graphql_movies/movies/schema.py
@@ -7,9 +7,16 @@ class ActorType(DjangoObjectType):
     class Meta:
         model = Actor
 
-
 # Create a Graphql type for the movie model
 class MovieType(DjangoObjectType):
     class Meta:
         model = Movie
 
+# Create a Query type
+class Query(ObjectType):
+    actor = graphene.Field(ActorType, id=graphene.Int())
+    movie = graphene.Field(MovieType, id=graphene.Int())
+    actors = graphene.Field(ActorType)
+    movies = graphene.Field(MovieType)
+
+    
